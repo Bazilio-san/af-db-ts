ea7d828766a5a38092c8fc639a74b435cb65b4e7
vvmakarov <vvmakarov@corp.finam.ru>
2023-12-09 19:33:47 +0300

    2.0.76

--- src/pg/utils-pg.ts
+++ src/pg/utils-pg.ts
@@ -2,29 +2,90 @@ import { TDataTypePg, TArrayTypesPg } from '../@types/i-data-types-pg';
 
 export const getJsTypeByTypePg = (dataType?: TDataTypePg, arrayType?: TDataTypePg): string => {
   switch (dataType) {
+    case 'bit':
+    case 'bool':
     case 'boolean':
       return 'boolean';
+
     case 'bigint':
       return 'string | number';
-    case 'integer':
-    case 'numeric':
-    case 'real':
+
     case 'smallint':
+    case 'int2':
+    case 'int':
+    case 'integer':
+    case 'int4':
+    case 'int8':
+    case 'numeric':
+    case 'decimal':
+    case 'real':
+    case 'float4':
+    case 'money':
+    case 'double precision':
+    case 'float8':
       return 'number';
-    case 'text':
+
+    case 'smallserial':
+    case 'serial2':
+    case 'serial':
+    case 'serial4':
+    case 'bigserial':
+    case 'serial8':
+      return 'number';
+
     case 'character':
+    case 'char':
     case 'varchar':
+    case 'character varying':
+    case 'string':
+    case 'text':
     case 'uuid':
+    case 'xml':
       return 'string';
+
+    case 'bytea':
+      return 'string';
+
     case 'json':
     case 'jsonb':
       return 'any';
+
     case 'date':
+    case 'time':
     case 'timestamptz':
+    case 'timestamp with time zone':
     case 'timestamp':
+    case 'timestamp without time zone':
+    case 'time without time zone':
+    case 'timetz':
+    case 'time with time zone':
       return 'string | Date | number';
+
     case 'USER_DEFINED':
       return 'string';
+
+    // Не задействованы
+    case 'bit varying':
+    case 'varbit':
+    case 'box': // rectangular box on a plane
+    case 'cidr': // IPv4 or IPv6 network address
+    case 'circle': // circle on a plane
+    case 'inet': // IPv4 or IPv6 host address
+    case 'line': // infinite line on a plane
+    case 'interval': // interval [fields][(p)] // time span
+    case 'lseg': // line segment on a plane
+    case 'macaddr': // MAC (Media Access Control) address
+    case 'macaddr8': // MAC (Media Access Control) address (EUI-64 format)
+    case 'path': // geometric path on a plane
+    case 'pg_lsn': // PostgreSQL Log Sequence Number
+    case 'pg_snapshot': // user-level transaction ID snapshot
+    case 'point': // geometric point on a plane
+    case 'polygon': // closed geometric path on a plane
+    case 'tsquery': // text search query
+    case 'tsvector': // text search document
+    case 'txid_snapshot': // user-level transaction ID snapshot (deprecated; see pg_snapshot)
+      return 'string';
+
     case 'ARRAY': {
       const jsType = getJsTypeByTypePg(arrayType);
       return `${jsType}[]`;
