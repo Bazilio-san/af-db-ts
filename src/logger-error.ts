7c9feb4799698776e1fcb15b1bf6b73a59a741b2
vvmakarov <vvmakarov@corp.finam.ru>
2026-01-22 22:46:28 +0300

    3.0.24

--- src/logger-error.ts
+++ src/logger-error.ts
@@ -1,10 +1,8 @@
-import { echo } from 'af-echo-ts';
-
 interface IAsLogger {
   error: Function,
 }
 
-export const logger: IAsLogger = echo as IAsLogger;
+export const logger: IAsLogger = console as IAsLogger;
 
 export const setLogger = (logger_: any) => {
   logger.error = logger_.error.bind(logger_);
